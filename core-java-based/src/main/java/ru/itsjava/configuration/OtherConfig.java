a92fef98d067fc11c214ec3e2dc3af7379dab8c0
Sols7 <serg-lex@yandex.ru>
2022-02-02 23:54:28 +0300

    module core-java-based version 1

---
+++ core-java-based/src/main/java/ru/itsjava/configuration/OtherConfig.java
@@ -0,0 +1,14 @@
+package ru.itsjava.configuration;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import ru.itsjava.Animal;
+import ru.itsjava.KindFarmer;
+
+public class OtherConfig {
+
+    @Bean
+    public KindFarmer farmer(@Qualifier("piglet") Animal animal){
+        return new KindFarmer(animal);
+    }
+}
